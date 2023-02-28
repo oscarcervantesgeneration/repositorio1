ceacf0d5f65ef7590d7dccbab63d0c47b678ffb0
oscarcervantesgeneration <oscarcervantes1420@gmail.com>
2023-02-28 17:38:00 -0600

    28 de febrero java

---
+++ 05Java/Java-05/src/principal/TestEstructuraMultiple.java
@@ -0,0 +1,10 @@
+package principal;
+
+import control.Estructuramultiple;
+
+public class TestEstructuraMultiple {
+	public static void main(String[] args) {
+		Estructuramultiple em = new Estructuramultiple();
+		em.ternario();
+	}
+}
